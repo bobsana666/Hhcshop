035f953e59a7a36d2b21493f073a2eb13eec6928
Borislav Binev <bbinev@taxback.com>
2023-09-09 09:35:56 +0300

    Appview close test(withMessage)

---
+++ messageResponser.php
@@ -0,0 +1,11 @@
+<?php
+use Telegram\Bot\Api;
+
+$telegram = new Api('5902538342:AAGXGYYJdPF58tVr1twKSQ7Q-pQ-B_IpuRw');
+
+$response = $telegram->sendMessage([
+  'chat_id' => '@littlefinger', 
+  'text' => 'Hello World'
+]);
+
+$messageId = $response->getMessageId();
