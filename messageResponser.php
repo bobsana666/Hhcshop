cdaf95e9f5e2e54badda87f5fdae80b35d5c5940
Borislav Binev <bbinev@taxback.com>
2023-09-09 09:37:34 +0300

    Appview close test(withMessage)

--- messageResponser.php
+++ messageResponser.php
@@ -4,7 +4,7 @@ use Telegram\Bot\Api;
 $telegram = new Api('5902538342:AAGXGYYJdPF58tVr1twKSQ7Q-pQ-B_IpuRw');
 
 $response = $telegram->sendMessage([
-  'chat_id' => '@littlefinger', 
+  'chat_id' => '@@hhcLiquidsWorld', 
   'text' => 'Hello World'
 ]);
 
